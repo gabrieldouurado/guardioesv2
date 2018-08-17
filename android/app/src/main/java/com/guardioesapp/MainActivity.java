275a16e9e4515e95d0d5da161d556754c8476f62
Foxtrot40 <cabeceiramarcos@gmail.com>
2018-08-17 16:33:39 -0300

    Facebook login modifications

--- android/app/src/main/java/com/guardioesapp/MainActivity.java
+++ android/app/src/main/java/com/guardioesapp/MainActivity.java
@@ -1,5 +1,6 @@
-package com.guardioesapp;
+package com.GuardioesApp;
 
+import android.content.Intent;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +13,10 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "GuardioesApp";
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
 }
