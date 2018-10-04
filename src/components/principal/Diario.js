b594ec90783667cf9697a2aa9eb37fa65b734b29
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2018-10-04 14:15:05 -0300

    Doing the chart here

--- src/components/principal/Diario.js
+++ src/components/principal/Diario.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text, AsyncStorage, ScrollView, StyleSheet, Image, ImageBackground } from 'react-native';
+import {
+  View, Text, AsyncStorage, ScrollView, StyleSheet, Image, ImageBackground,
+  processColor, LayoutAnimation, AppRegistry
+} from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import moment from 'moment';
 import { Avatar } from 'react-native-elements'
 import { imagemBad, imagemGood, imagemFundo } from '../../imgs/imageConst';
+import { Dimensions } from 'react-native'
+import update from "immutability-helper"
+import { LineChart } from "react-native-charts-wrapper";
 
+const greenBlue = "rgb(26, 182, 151)";
+const petrel = "rgb(59, 145, 153)";
 
 let data = new Date();
 let d = data.getDate();
@@ -17,6 +25,7 @@ let today = y + "-" + m + "-" + d + "T" + h;
 const _format = 'YYYY-MM-DD'
 const _today = moment().format(_format)
 
+
 class Diario extends Component {
   constructor(props) {
     super(props);
@@ -26,16 +35,30 @@ class Diario extends Component {
       datesMarked: {
 
       },
-      pic: "http://www.politize.com.br/wp-content/uploads/2016/08/imagem-sem-foto-de-perfil-do-facebook-1348864936180_956x5001.jpg"
+      pic: "http://www.politize.com.br/wp-content/uploads/2016/08/imagem-sem-foto-de-perfil-do-facebook-1348864936180_956x5001.jpg",
+      BadData: [],
+      BadPlot: [{y:0, x:0, marked: ""}],
+      NoPlot: [{y:0, x:0, marked: ""}]
     };
   }
+
+  handleSelect(event) {
+    let entry = event.nativeEvent;
+    if (entry == null) {
+      this.setState({ ...this.state, selectedEntry: null });
+    } else {
+      this.setState({ ...this.state, selectedEntry: JSON.stringify(entry) });
+    }
+
+    console.log(event.nativeEvent);
+  }
+
   static navigationOptions = {
     title: 'Diário Da Saúde',
   }
 
   async GetUserData() {
     let userID = await AsyncStorage.getItem('userID');
-    console.log(userID)
     let userName = await AsyncStorage.getItem('userName');
     let pic = await AsyncStorage.getItem('avatar');
     this.setState({
@@ -46,27 +69,26 @@ class Diario extends Component {
     let url = `https://guardianes.centeias.net/user/surveys/${this.state.userID}`
     return fetch(url
       , {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
       .then((response) => response.json())
       .then((responseJson) => {
         if (responseJson.error === false) {
           this.setState({ data: responseJson.data });
           this.defineMarkedDates()
-          
         }
       })
   }
 
-  componentWillMount() {
-    this.GetUserData()
+  componentDidMount() {
+    this.GetUserData();
   }
 
 
-    defineMarkedDates = () => {
+  defineMarkedDates = () => {
 
     let dataAux = this.state.data
     let markedDateNo = []
@@ -90,8 +112,10 @@ class Diario extends Component {
       GoodPercent: GoodPercent,
       BadPercent: BadPercent,
       totalReports: totalReports,
-      markedDateNo: markedDateNo.length,
-      markedDate: markedDate.length
+      NummarkedDateNo: markedDateNo.length,
+      NummarkedDate: markedDate.length,
+      markedDate: markedDate,
+      markedDateNo: markedDateNo,
     })
 
     let BadReport = markedDate.reduce((c, v) => Object.assign(c, { [v]: { selected: true, selectedColor: '#64C2D4' } }), {});
@@ -100,11 +124,135 @@ class Diario extends Component {
     Object.assign(GoodReport, BadReport);
 
     this.setState({ datesMarked: GoodReport });
+    this.ChartData();
+  }
 
+  ChartData = () => {
+    let badData = this.state.markedDate;
+    let NoSymptomData = this.state.markedDateNo
+    let BadDataAux = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let NoDataAux = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    badData.map((data, index) => {
+      // YYYY-MM-DD
+      switch (data.split("-")[1]) {
+        case "01":
+          BadDataAux[0]++
+          break;
+        case "02":
+          BadDataAux[1]++
+          break;
+        case "03":
+          BadDataAux[2]++
+          break;
+        case "04":
+          BadDataAux[3]++
+          break;
+        case "05":
+          BadDataAux[4]++
+          break;
+        case "06":
+          BadDataAux[5]++
+          break;
+        case "07":
+          BadDataAux[6]++
+          break;
+        case "08":
+          BadDataAux[7]++
+          break;
+        case "09":
+          BadDataAux[8]++
+          break;
+        case "10":
+          BadDataAux[9]++
+          break;
+        case "11":
+          BadDataAux[10]++
+          break;
+        case "12":
+          BadDataAux[11]++
+          break;
+      }
+    })
+    var BadPlotAux = [
+      { y: BadDataAux[0], x: 0, marked: 'No mês de Janeiro tivemos ' + BadDataAux[0] },
+      { y: BadDataAux[1], x: 1, marked: 'No mês de Fevereiro tivemos ' + BadDataAux[1]  },
+      { y: BadDataAux[2], x: 2, marked: 'No mês de Março tivemos ' + BadDataAux[2]   },
+      { y: BadDataAux[3], x: 3, marked: 'No mês de Abril tivemos ' + BadDataAux[3]   },
+      { y: BadDataAux[4], x: 4, marked: 'No mês de Maio tivemos ' + BadDataAux[4]   },
+      { y: BadDataAux[5], x: 5, marked: 'No mês de Junho tivemos ' + BadDataAux[5]   },
+      { y: BadDataAux[6], x: 6, marked: 'No mês de Julho tivemos ' + BadDataAux[6]   },
+      { y: BadDataAux[7], x: 7, marked: 'No mês de Agosto tivemos ' + BadDataAux[7]   },
+      { y: BadDataAux[8], x: 8, marked: 'No mês de Setembrom tivemos ' + BadDataAux[8]   },
+      { y: BadDataAux[9], x: 9, marked: 'No mês de Outubro tivemos ' + BadDataAux[9]   },
+      { y: BadDataAux[10], x: 10, marked: 'No mês de Novembro tivemos ' + BadDataAux[10]   },
+      { y: BadDataAux[11], x: 11, marked: 'No mês de Dezembro tivemos ' + BadDataAux[11]   }]
+
+      NoSymptomData.map((data, index) => {
+        // YYYY-MM-DD
+        switch (data.split("-")[1]) {
+          case "01":
+            NoDataAux[0]++
+            break;
+          case "02":
+            NoDataAux[1]++
+            break;
+          case "03":
+            NoDataAux[2]++
+            break;
+          case "04":
+            NoDataAux[3]++
+            break;
+          case "05":
+            NoDataAux[4]++
+            break;
+          case "06":
+            NoDataAux[5]++
+            break;
+          case "07":
+            NoDataAux[6]++
+            break;
+          case "08":
+            NoDataAux[7]++
+            break;
+          case "09":
+            NoDataAux[8]++
+            break;
+          case "10":
+            NoDataAux[9]++
+            break;
+          case "11":
+            NoDataAux[10]++
+            break;
+          case "12":
+            NoDataAux[11]++
+            break;
+        }
+      })
+
+      var NoPlotAux = [
+        { y: NoDataAux[0], x: 0, marked: 'No mês de Janeiro tivemos ' + NoDataAux[0] },
+        { y: NoDataAux[1], x: 1, marked: 'No mês de Fevereiro tivemos ' + NoDataAux[1]  },
+        { y: NoDataAux[2], x: 2, marked: 'No mês de Março tivemos ' + NoDataAux[2]   },
+        { y: NoDataAux[3], x: 3, marked: 'No mês de Abril tivemos ' + NoDataAux[3]   },
+        { y: NoDataAux[4], x: 4, marked: 'No mês de Maio tivemos ' + NoDataAux[4]   },
+        { y: NoDataAux[5], x: 5, marked: 'No mês de Junho tivemos ' + NoDataAux[5]   },
+        { y: NoDataAux[6], x: 6, marked: 'No mês de Julho tivemos ' + NoDataAux[6]   },
+        { y: NoDataAux[7], x: 7, marked: 'No mês de Agosto tivemos ' + NoDataAux[7]   },
+        { y: NoDataAux[8], x: 8, marked: 'No mês de Setembrom tivemos ' + NoDataAux[8]   },
+        { y: NoDataAux[9], x: 9, marked: 'No mês de Outubro tivemos ' + NoDataAux[9]   },
+        { y: NoDataAux[10], x: 10, marked: 'No mês de Novembro tivemos ' + NoDataAux[10]   },
+        { y: NoDataAux[11], x: 11, marked: 'No mês de Dezembro tivemos ' + NoDataAux[11]   }]
+
+
+    this.setState({ BadData: BadDataAux,
+                    BadPlot: BadPlotAux,
+                    NoPlot: NoPlotAux});
   }
 
   render() {
-
+    const AUXdata = this.state.BadData
+    const BadplotAux = this.state.BadPlot
+    const NoPlotAux = this.state.NoPlot
     return (
       <ScrollView>
         <ImageBackground style={styles.container} imageStyle={{ resizeMode: 'center', marginLeft: '5%', marginRight: '5%' }} source={imagemFundo}>
@@ -115,7 +263,7 @@ class Diario extends Component {
                 rounded
                 source={{ uri: this.state.pic }}
                 activeOpacity={0.7}
-                style={{borderWidth: 1, borderColor: 'red',}}
+                style={{ borderWidth: 1, borderColor: 'red', }}
               />
               <Text style={styles.UserName}>
                 {this.state.userName}
@@ -127,25 +275,139 @@ class Diario extends Component {
                 <Image style={{ width: 35, height: 35, paddingRight: 10 }} source={imagemGood} />
                 <View style={styles.columnData}>
                   <Text style={styles.GoodPercent}>{this.state.GoodPercent}% Bem</Text>
-                  <Text style={styles.numGood}> {this.state.markedDateNo === 1 ? this.state.markedDateNo + ' informe' : this.state.markedDateNo + ' informes'}</Text>
+                  <Text style={styles.numGood}> {this.state.NummarkedDateNo === 1 ? this.state.NummarkedDateNo + ' informe' : this.state.NummarkedDateNo + ' informes'}</Text>
                 </View>
               </View>
               <View style={styles.BadData}>
                 <Image style={{ width: 35, height: 35, paddingRight: 10 }} source={imagemBad} />
                 <View style={styles.columnData}>
                   <Text style={styles.BadPercent}>{this.state.BadPercent}% Mal</Text>
-                  <Text style={styles.numBad}>{this.state.markedDate === 1 ? this.state.markedDate + ' informe' : this.state.markedDate + ' informes'}</Text>
+                  <Text style={styles.numBad}>{this.state.NummarkedDate === 1 ? this.state.NummarkedDate + ' informe' : this.state.NummarkedDate + ' informes'}</Text>
                 </View>
               </View>
             </View>
           </View>
           <View style={styles.CalendarDate}>
-          <View style={styles.ViewCalendario}><Text style={styles.Calendario}>Calendário</Text></View>
-          <Calendar
-            current={_today}
-            markedDates={this.state.datesMarked}
-          />
-         </View>
+            <View style={styles.ViewCalendario}><Text style={styles.Calendario}>Calendário</Text></View>
+            <Calendar
+              current={_today}
+              markedDates={this.state.datesMarked}
+            />
+          </View>
+          <View style={styles.chartView}>
+            <View style={styles.ViewCalendario}><Text style={styles.Calendario}>Gráfico</Text></View>
+
+
+            <LineChart
+              style={styles.chart}
+              data={{
+                dataSets: [
+                  {
+                    values: BadplotAux,
+                    label: "",
+                    config: {
+                      mode: "CUBIC_BEZIER",
+                      drawValues: false,
+                      lineWidth: 2,
+                      drawCircles: true,
+                      circleColor: processColor(petrel),
+                      drawCircleHole: false,
+                      circleRadius: 5,
+                      highlightColor: processColor("trasparent"),
+                      color: processColor('#64C2D4'),
+                      drawFilled: true,
+                      // fillGradient: {
+                      //   colors: [processColor("#64C2D4"), processColor("#64C2D4")],
+                      //   positions: [0, 0.5],
+                      //   angle: 90,
+                      //   orientation: "TOP_BOTTOM"
+                      // },
+                      fillAlpha: 1000,
+                      valueTextSize: 15
+                    }
+                  },
+
+                  {
+                    values: NoPlotAux,
+                    label: "",
+                    config: {
+                      mode: "CUBIC_BEZIER",
+                      drawValues: false,
+                      lineWidth: 2,
+                      drawCircles: true,
+                      circleColor: processColor(greenBlue),
+                      drawCircleHole: false,
+                      circleRadius: 5,
+                      highlightColor: processColor("transparent"),
+                      color: processColor( '#C19036'),
+                      drawFilled: true,
+                      // fillGradient: {
+                      //   colors: [processColor("#64C2D4"), processColor("#64C2D4")],
+                      //   positions: [0, 0.5],
+                      //   angle: 90,
+                      //   orientation: "TOP_BOTTOM"
+                      // },
+                      fillAlpha: 1000,
+                      valueTextSize: 15
+                    }
+                  },
+                ]
+              }}
+              chartDescription={{ text: "" }}
+              legend={{
+                enabled: false
+              }}
+              marker={{
+                enabled: true,
+                markerColor: processColor("white"),
+                textColor: processColor("black")
+              }}
+              xAxis={{
+                enabled: true,
+                granularity: 1,
+                drawLabels: true,
+                position: "BOTTOM",
+                drawAxisLine: true,
+                drawGridLines: false,
+                fontFamily: "HelveticaNeue-Medium",
+                fontWeight: "bold",
+                textSize: 12,
+                textColor: processColor("gray"),
+                valueFormatter: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+              }}
+              yAxis={{
+                left: {
+                  enabled: true
+                },
+                right: {
+                  enabled: false
+                }
+              }}
+              autoScaleMinMaxEnabled={true}
+              animation={{
+                durationX: 0,
+                durationY: 1500,
+                easingY: "EaseInOutQuart"
+              }}
+              drawGridBackground={false}
+              drawBorders={false}
+              touchEnabled={true}
+              dragEnabled={false}
+              scaleEnabled={false}
+              scaleXEnabled={false}
+              scaleYEnabled={false}
+              pinchZoom={false}
+              doubleTapToZoomEnabled={false}
+              dragDecelerationEnabled={true}
+              dragDecelerationFrictionCoef={0.99}
+              keepPositionOnRotation={false}
+              onSelect={this.handleSelect.bind(this)}
+              onChange={event => console.log(event.nativeEvent)}
+            />
+          </View>
+          <View style={styles.ViewCalendario}><Text style={styles.Calendario}>Gráfico</Text></View>
+          <View style={styles.ViewCalendario}><Text style={styles.Calendario}>Gráfico</Text></View>
+          <View style={styles.ViewCalendario}><Text style={styles.Calendario}>Gráfico</Text></View>
         </ImageBackground>
       </ScrollView>
     );
@@ -226,6 +488,13 @@ const styles = StyleSheet.create({
   },
   CalendarDate: {
     marginBottom: 80,
+  },
+  chartView: {
+    flex: 1,
+    padding: 20
+},
+  chart: {
+    height: Dimensions.get('window').height
   }
 });
 export default Diario;
