70a7cda3984f5b1c8af13a81395c72cdd3ba048e
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2018-10-04 12:43:08 -0300

    Testing

--- App.js
+++ App.js
@@ -1,48 +1,13 @@
-//import React, { Component } from 'react';
-//import * as Navegar from './src/components/navigation/navigator';
+import React, { Component } from 'react';
+import * as Navegar from './src/components/navigation/navigator';
 
-//class Guardioes extends Component {
-//  render() {
-//    return (
- //     <Navegar.Drawer />
-  //  );
-  //}
-//}
-
-
-//export default Guardioes;
-
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View, processColor
-} from 'react-native';
-
-import {LineChart} from 'react-native-charts-wrapper';
-
-export default class App extends React.Component {
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <View style={styles.container}>
-          <LineChart style={styles.chart}
-            data={{dataSets:[{label: "demo", values: [{y: 1}, {y: 2}, {y: 1}]}]}}
-          />
-        </View>
-      </View>
-    );
+class Guardioes extends Component {
+ render() {
+   return (
+     <Navegar.Drawer />
+   );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF'
-  },
-  chart: {
-    flex: 1
-  }
-});
+
+export default Guardioes;
