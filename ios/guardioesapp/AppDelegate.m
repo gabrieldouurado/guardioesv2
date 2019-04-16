a9b52810762251b4505847d344607e5cf6f1292d
ProEpi <proepi@iMac-de-Vitor.local>
2019-04-16 11:13:40 -0300

    undo stash

--- ios/guardioesapp/AppDelegate.m
+++ ios/guardioesapp/AppDelegate.m
@@ -9,26 +9,17 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @implementation AppDelegate
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-  [FBSDKAppEvents activateApp];
-}
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  
-  [[FBSDKApplicationDelegate sharedInstance] application:application
-                           didFinishLaunchingWithOptions:launchOptions];
-  
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"guardioesapp"
+                                                      moduleName:@"GuardioesApp"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
@@ -39,16 +30,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication
-         annotation:(id)annotation {
-  return [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                        openURL:url
-                                              sourceApplication:sourceApplication
-                                                     annotation:annotation];
 }
 
 @end
---
+++ src/components/telainicial/Loading.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  ActivityIndicator, AsyncStorage, Image, StatusBar, StyleSheet} from 'react-native';
+import { imagemLogo } from '../../imgs/imageConst';
+import LinearGradient from 'react-native-linear-gradient';
+
+class AuthLoadingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    let UserID = await AsyncStorage.getItem('userID');
+
+    if (UserID !== null) {
+      setTimeout(() => {
+        this.props.navigation.navigate('BottomMenu');
+      }, 1500);
+    } else {
+      this.props.navigation.navigate('Cadastro');
+    }
+  };
+
+  // Render any loading content that you like here
+  render() {
+    const statusColor = (<StatusBar backgroundColor='#348EAC' />)
+    return (
+      <LinearGradient style={styles.container} colors={['#348EAC', '#013444']} start={{ x: 1.5, y: 0.6 }} end={{ x: -0.2, y: 1.4 }}>        
+          {statusColor}
+          <Image style={styles.imageLogo} source={imagemLogo} />
+          <ActivityIndicator size="large" />
+          <StatusBar barStyle="default" />        
+      </LinearGradient>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageLogo: {
+    marginBottom: 20,
+    height: '35%',
+    resizeMode: 'center',
+  }
+});
+
+export default AuthLoadingScreen;
